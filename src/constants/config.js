08c265bc1f46743a913d98c181dd27ee071cdbb6
razeev990 <razeevshah990@gmail.com>
2026-09-14 16:10:56 +0545

    Delete src directory

--- src/constants/config.js
+++
@@ -1,5 +0,0 @@
-export const APP_NAME = 'Ludo Supreme';
-
-export const APP_VERSION = '1.0.0';
-
-export const DEBUG_MODE = true;
--- src/utils/diceUtils.js
+++
@@ -1,7 +0,0 @@
-export const rollDice = () => {
-  return (
-    Math.floor(
-      Math.random() * 6
-    ) + 1
-  );
-};
